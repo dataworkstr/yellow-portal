197976608a40be5353bdd2520c6e598e4e73174a
unknown <muratbayri@Toshiba.YELLOWPAGES.COM>
2015-07-27 16:20:08 +0300

    oyuncu ve takım sayfaları değiştirdim

--- pages/players.php
+++ pages/players.php
@@ -6,61 +6,21 @@
 
 <div class="news-header"> <h3><i class="fa fa-th-list"></i> Oyuncular</h3></div>
 
-   <div class="col-xs-6">
-
-    <div class="oyuncu-liste">
-       <img src="images/fenerlogo.png" width="100" height="100"/>  <br/>
-       <p>Körükspor</p>
-        <div class="oyuncu-adi">
 
 
-            <table class="table">
-
-                      <tbody>
-                        <tr>
-                          <th scope="row">1</th>
-                          <td><div class="sakatlanmis-oyuncu">Skt</div></td>
-                          <td>OZAN</td>
-                        </tr>
-
-                        <tr>
-                          <th scope="row">2</th>
-                          <td><div class="yabanci-oyuncu">Ybn</div></td>
-                          <td>RONALDO</td>
-                        </tr>
-
-                           <tr>
-                          <th scope="row">2</th>
-                          <td><div class="yerli-oyuncu">Yrl</div></td>
-                          <td>RONALDO</td>
-                        </tr>
-
-                      </tbody>
-            </table>
 
 
-        </div>
+<div class="oyuncu-liste">
+        <div class="oyuncuresim"><a href="#"><img src="images/oyuncu/ronaldinho.jpg" width="105" height="120"></a></div><br>
+        <div class="oyuncu-adi"><a href="#">Ronaldinho</a></div>
     </div>
 
-
-    </div>
-
-
-     <div class="col-xs-6">
-
-    <div class="oyuncu-liste">
-         <img src="images/fenerlogo.png" width="100" height="100"/>  <br/>
-       <p>AAÜ Ejderhaspor</p>
-       <hr/>
-        <div class="oyuncu-adi">Ronaldinho</div>
-    </div>
-
-
-    </div>
 
 
 
 </div>
+
+<?php include "pages/yapim.php"; ?>
 
 </div>
 
--- pages/teams.php
+++ pages/teams.php
@@ -2,24 +2,88 @@
 <div class="col-md-9">
 
 
-    <div class="takim-sayfasi">
+    <div class="haberler-sayfasi">
 
 <div class="news-header"> <h3><i class="fa fa-th-list"></i> Takımlar</h3></div>
 
-    <div class="takimlar2">
-        <img src="images/fenerlogo.png" width="200" height="200"/>  <br/>
-        <div class="takimismi">Körükspor</div>
-    </div>
+<div class="col-xs-6">
+<div class="oyuncu-liste">
+       <img src="images/fenerlogo.png" width="100" height="100"/>  <br/>
+       <p>Körükspor</p>
 
-     <div class="takimlar2">
-        <img src="images/fenerlogo.png" width="200" height="200"/>  <br/>
-        <div class="takimismi">AAÜ Ejderhaspor</div>
+        <div class="oyuncu-adi">
+
+
+            <table class="table">
+
+                      <tbody>
+                        <tr>
+                          <th scope="row">1</th>
+                          <td><div class="sakatlanmis-oyuncu">Skt</div></td>
+                          <td>OZAN</td>
+                        </tr>
+
+                        <tr>
+                          <th scope="row">2</th>
+                          <td><div class="yabanci-oyuncu">Ybn</div></td>
+                          <td>RONALDO</td>
+                        </tr>
+
+                           <tr>
+                          <th scope="row">2</th>
+                          <td><div class="yerli-oyuncu">Yrl</div></td>
+                          <td>RONALDO</td>
+                        </tr>
+
+                      </tbody>
+            </table>
+
+
+        </div>
     </div>
+        </div>
+
+         <div class="col-xs-6">
+             <div class="oyuncu-liste">
+       <img src="images/fenerlogo.png" width="100" height="100"/>  <br/>
+       <p>Körükspor</p>
+
+        <div class="oyuncu-adi">
+
+
+            <table class="table">
+
+                      <tbody>
+                        <tr>
+                          <th scope="row">1</th>
+                          <td><div class="sakatlanmis-oyuncu">Skt</div></td>
+                          <td>OZAN</td>
+                        </tr>
+
+                        <tr>
+                          <th scope="row">2</th>
+                          <td><div class="yabanci-oyuncu">Ybn</div></td>
+                          <td>RONALDO</td>
+                        </tr>
+
+                           <tr>
+                          <th scope="row">2</th>
+                          <td><div class="yerli-oyuncu">Yrl</div></td>
+                          <td>RONALDO</td>
+                        </tr>
+
+                      </tbody>
+            </table>
+
+
+        </div>
+    </div>
+        </div>
 
 
 </div>
 
-<?php include "pages/yapim.php"; ?>
+
 
 
 </div>
