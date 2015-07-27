3cef6854105298b217e547311c2ae14b2311fb60
unknown <muratbayri@Toshiba.YELLOWPAGES.COM>
2015-07-27 13:34:35 +0300

    oyuncular sayfası düzenlendi

--- pages/players.php
+++ pages/players.php
@@ -6,86 +6,30 @@
 
 <div class="news-header"> <h3><i class="fa fa-th-list"></i> Oyuncular</h3></div>
 
-    <div class="oyuncu-liste">
-        <div class="oyuncuresim"><a href="#"><img src="images/oyuncu/ronaldinho.jpg" width="105" height="120"/></a></div><br/>
-        <div class="oyuncu-adi"><a href="#">Ronaldinho</a></div>
-    </div>
+   <div class="col-xs-6">
 
     <div class="oyuncu-liste">
-        <div class="oyuncuresim"><img src="images/oyuncu/ronaldinho.jpg" width="105" height="120"/></div><br/>
+       <img src="images/fenerlogo.png" width="100" height="100"/>  <br/>
+       <div id="adi">Körükspor</div>
+       <hr/>
         <div class="oyuncu-adi">Ronaldinho</div>
     </div>
+
+
+    </div>
+
+
+     <div class="col-xs-6">
 
     <div class="oyuncu-liste">
-        <div class="oyuncuresim"><img src="images/oyuncu/ronaldinho.jpg" width="105" height="120"/></div><br/>
-        <div class="oyuncu-adi">Ronaldinho</div>
-    </div>
-
-    <div class="oyuncu-liste">
-        <div class="oyuncuresim"><img src="images/oyuncu/ronaldinho.jpg" width="105" height="120"/></div><br/>
-        <div class="oyuncu-adi">Ronaldinho</div>
-    </div>
-
-    <div class="oyuncu-liste">
-        <div class="oyuncuresim"><img src="images/oyuncu/ronaldinho.jpg" width="105" height="120"/></div><br/>
+         <img src="images/fenerlogo.png" width="100" height="100"/>  <br/>
+       <div id="adi">AAÜ Ejderhaspor</div>
+       <hr/>
         <div class="oyuncu-adi">Ronaldinho</div>
     </div>
 
 
-     <div class="oyuncu-liste">
-        <div class="oyuncuresim"><img src="images/oyuncu/ronaldinho.jpg" width="105" height="120"/></div><br/>
-        <div class="oyuncu-adi">Ronaldinho</div>
     </div>
-
-    <div class="oyuncu-liste">
-        <div class="oyuncuresim"><img src="images/oyuncu/ronaldinho.jpg" width="105" height="120"/></div><br/>
-        <div class="oyuncu-adi">Ronaldinho</div>
-    </div>
-
-     <div class="oyuncu-liste">
-        <div class="oyuncuresim"><img src="images/oyuncu/ronaldinho.jpg" width="105" height="120"/></div><br/>
-        <div class="oyuncu-adi">Ronaldinho</div>
-    </div>
-
-    <div class="oyuncu-liste">
-        <div class="oyuncuresim"><img src="images/oyuncu/ronaldinho.jpg" width="105" height="120"/></div><br/>
-        <div class="oyuncu-adi">Ronaldinho</div>
-    </div>
-
-     <div class="oyuncu-liste">
-        <div class="oyuncuresim"><img src="images/oyuncu/ronaldinho.jpg" width="105" height="120"/></div><br/>
-        <div class="oyuncu-adi">Ronaldinho</div>
-    </div>
-
-
-    <div class="oyuncu-liste">
-        <div class="oyuncuresim"><img src="images/oyuncu/ronaldinho.jpg" width="105" height="120"/></div><br/>
-        <div class="oyuncu-adi">Ronaldinho</div>
-    </div>
-
-     <div class="oyuncu-liste">
-        <div class="oyuncuresim"><img src="images/oyuncu/ronaldinho.jpg" width="105" height="120"/></div><br/>
-        <div class="oyuncu-adi">Ronaldinho</div>
-    </div>
-
-    <div class="oyuncu-liste">
-        <div class="oyuncuresim"><img src="images/oyuncu/ronaldinho.jpg" width="105" height="120"/></div><br/>
-        <div class="oyuncu-adi">Ronaldinho</div>
-    </div>
-
-     <div class="oyuncu-liste">
-        <div class="oyuncuresim"><img src="images/oyuncu/ronaldinho.jpg" width="105" height="120"/></div><br/>
-        <div class="oyuncu-adi">Ronaldinho</div>
-    </div>
-
-    <div class="oyuncu-liste">
-        <div class="oyuncuresim"><img src="images/oyuncu/ronaldinho.jpg" width="105" height="120"/></div><br/>
-        <div class="oyuncu-adi">Ronaldinho</div>
-    </div>
-
-
-
-
 
 
 
