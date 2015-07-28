ccde4c02c40806cf10137b8b8e6e4fec87cdbaa1
unknown <muratbayri@Toshiba.YELLOWPAGES.COM>
2015-07-28 09:18:58 +0300

    oyuncu detay sayfası düzenlendi

--- pages/oyuncu-detay.php
+++ pages/oyuncu-detay.php
@@ -7,7 +7,39 @@
 <div class="news-header"> <h3><i class="fa fa-newspaper-o"></i> Oyuncu Bilgileri</h3></div>
 
 
+<div class="oyuncu-sol-resim">
+    <img src="images/oyuncu/ronaldinho.jpg" width="246" height="300"/>
+</div>
 
+<div class="oyuncu-detay-yazi">
+    <h1>MURATCAN TAVUKÇUOĞLU</h1>
+
+    <b>Kişisel Bilgiler</b>
+    <hr/>
+    <ul>
+    <li>  <label>Doğum Yeri :</label> İstanbul</li>
+
+    <li>  <label>Doğum Tarihi :</label> 27.08.1988</li>
+
+    <li>  <label>Uyruk :</label> T.C.</li>
+
+    </ul>
+
+    <br/><br/>
+    <b>Lisans Bilgiler</b>
+    <hr/>
+    <ul>
+
+    <li>  <label>Kulübü :</label> Körükspor</li>
+
+    <li>  <label>Pozisyon :</label> AM R/C/L</li>
+
+    <li>  <label>Değer :</label> 41.000.000 $</li>
+
+    </ul>
+    <div id="formadanumaram">27</div>
+
+</div>
 
 
 
