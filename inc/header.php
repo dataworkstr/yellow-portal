2ab3dc67ee87f7c431eccf2ae91a37d0d3768e8a
dualsaber <dualsaber88@gmail.com>
2015-07-28 13:45:00 +0300

    Title ayarlandi

--- inc/header.php
+++ inc/header.php
@@ -1,61 +1,61 @@
 <html>
 
 <head>
-        <meta charset="utf-8"/>
-        <title>Yellow Pages Portal</title>
-        <meta name="description" content="Yellow Pages Portal"/>     
-        <meta name="author" content="Murat Bayri"/>
-        <link rel="shortcut icon" href="images/favicon.ico" type="image/x-icon" />
-        <meta name="robots" content="ALL"/>
+  <meta charset="utf-8"/>
+  <title>Yellow Bulls Resmi Websitesi</title>
+  <meta name="description" content="Yellow Pages Portal"/>     
+  <meta name="author" content="Murat Bayri"/>
+  <link rel="shortcut icon" href="images/favicon.ico" type="image/x-icon" />
+  <meta name="robots" content="ALL"/>
 
-    <link href='http://fonts.googleapis.com/css?family=Open+Sans:400,300,600,700,800' rel='stylesheet' type='text/css'>
-    <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
-		
-		
-		<script src="http://code.jquery.com/jquery-latest.min.js" type="text/javascript"></script> 
-		<script src="https://code.jquery.com/ui/1.11.0/jquery-ui.min.js"></script>
-      
-		<script type="text/javascript" src="js/jssor.js"></script>
-        <script type="text/javascript" src="js/jssor.slider.js"></script>
-		
-		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
-		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap-theme.min.css">
-		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
-		
-		
-		<link rel="stylesheet" href="css/style.css">
-		
-		
+  <link href='http://fonts.googleapis.com/css?family=Open+Sans:400,300,600,700,800' rel='stylesheet' type='text/css'>
+  <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
+  
+  
+  <script src="http://code.jquery.com/jquery-latest.min.js" type="text/javascript"></script> 
+  <script src="https://code.jquery.com/ui/1.11.0/jquery-ui.min.js"></script>
+  
+  <script type="text/javascript" src="js/jssor.js"></script>
+  <script type="text/javascript" src="js/jssor.slider.js"></script>
+  
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap-theme.min.css">
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
+  
+  
+  <link rel="stylesheet" href="css/style.css">
+  
+  
 </head>
 
 <div id="tum-site" class="row">
 
-<!-- <div class="advertise-top"> </div> -->
+  <!-- <div class="advertise-top"> </div> -->
 
-<nav class="navbar navbar-default">
-  <div class="container-fluid">
-    <!-- Brand and toggle get grouped for better mobile display -->
-    <div class="navbar-header">
-      <a class="navbar-brand" href="index.php"><div class="header-logo"><img src="images/logo.png" width="92" height="86"/></div></a>
-    </div>
+  <nav class="navbar navbar-default">
+    <div class="container-fluid">
+      <!-- Brand and toggle get grouped for better mobile display -->
+      <div class="navbar-header">
+        <a class="navbar-brand" href="index.php"><div class="header-logo"><img src="images/logo.png" width="92" height="86"/></div></a>
+      </div>
 
-    <!-- Collect the nav links, forms, and other content for toggling -->
-    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-      <ul class="nav navbar-nav">
-        <li><a href="index.php"><div class="menu-home-icon"><i class="fa fa-home"></i></div> <span class="sr-only">(current)</span></a></li>
-        <li><a href="index.php?page=news">Haberler</a></li>
-		<li><a href="index.php?page=teams">Takımlar</a></li>
-		<li><a href="index.php?page=players">Oyuncular</a></li>
-		<li><a href="index.php?page=stadium">Sahalar</a></li>
-		
-      </ul>
-    
-   
-    </div><!-- /.navbar-collapse -->
-  </div><!-- /.container-fluid -->
-</nav>
+      <!-- Collect the nav links, forms, and other content for toggling -->
+      <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+        <ul class="nav navbar-nav">
+          <li><a href="index.php"><div class="menu-home-icon"><i class="fa fa-home"></i></div> <span class="sr-only">(current)</span></a></li>
+          <li><a href="index.php?page=news">Haberler</a></li>
+          <li><a href="index.php?page=teams">Takımlar</a></li>
+          <li><a href="index.php?page=players">Oyuncular</a></li>
+          <li><a href="index.php?page=stadium">Sahalar</a></li>
+          
+        </ul>
+        
+        
+      </div><!-- /.navbar-collapse -->
+    </div><!-- /.container-fluid -->
+  </nav>
 
-<!-- <div class="advertise-bottom"></div> -->
+  <!-- <div class="advertise-bottom"></div> -->
 
 
-<body>
+  <body>
