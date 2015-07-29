de170e7b36f8df63037223e699ae3a393163cd10
dualsaber <dualsaber88@gmail.com>
2015-07-29 13:24:45 +0300

    Klasör yapısı değiştirildi

--- inc/content.php
+++ inc/content.php
@@ -1,43 +1,43 @@
 <?php
-	$yellowcek = $_GET['page'];
-	
-	switch($yellowcek){
-		case "news" :
-		include "pages/news.php";
-		break;
+$yellowcek = $_GET['page'];
 
-            case "haber-detay" :
-            include "pages/haber-detay.php";
-            break;
-		
-		case "teams" :
-		include "pages/teams.php";
-		break;
+switch($yellowcek){
+	case "news" :
+	include "pages/news.php";
+	break;
 
-            case "takim-detay" :
-            include "pages/takim-detay.php";
-            break;
+	case "haber-detay" :
+	include "components/haber-detay.php";
+	break;
 
-		
-		case "players" :
-		include "pages/players.php";
-		break;
+	case "teams" :
+	include "pages/teams.php";
+	break;
 
-            case "oyuncu-detay" :
-            include "pages/oyuncu-detay.php";
-            break;
-		
-		case "stadium" :
-		include "pages/stadium.php";
-		break;
+	case "takim-detay" :
+	include "components/takim-detay.php";
+	break;
 
-            case "saha-detay" :
-            include "pages/saha-detay.php";
-            break;
-		
-		default :
-		include "pages/home.php";
-		break;
-	}
-	
+
+	case "players" :
+	include "pages/players.php";
+	break;
+
+	case "oyuncu-detay" :
+	include "components/oyuncu-detay.php";
+	break;
+
+	case "stadium" :
+	include "pages/stadium.php";
+	break;
+
+	case "saha-detay" :
+	include "components/saha-detay.php";
+	break;
+
+	default :
+	include "pages/home.php";
+	break;
+}
+
 ?>
