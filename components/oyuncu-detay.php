108dff649517214ff030034dfa2a41fef8023f1c
dualsaber <dualsaber88@gmail.com>
2015-07-30 11:09:19 +0300

    Oyuncu detaylar sayfasına oyuncu durumu eklendi

--- components/oyuncu-detay.php
+++ components/oyuncu-detay.php
@@ -1,65 +1,67 @@
 <div>
-<div class="col-md-9">
+    <div class="col-md-9">
 
 
-    <div class="haberler-sayfasi">
+        <div class="haberler-sayfasi">
 
-<div class="news-header"> <h3><i class="fa fa-newspaper-o"></i> Oyuncu Bilgileri</h3></div>
-
-
-
-<div class="oyuncu-sol-resim">
-    <img src="images/oyuncu/ronaldinho.jpg" width="246" height="300"/>
-</div>
-
-<div class="oyuncu-detay-yazi">
-
-
-    <h1>MURATCAN TAVUKÇUOĞLU</h1>
-
-    <b><div class="baslik-yani-uzatici"></div>Kişisel Bilgiler</b>
-    <hr/>
-    <ul>
-    <li>  <label>Doğum Yeri :</label> İstanbul</li>
-
-    <li>  <label>Boy :</label> 1.90 m</li>
-
-    <li>  <label>Kilo :</label> 90 kg</li>
-
-    <li>  <label>Yaş :</label> 25</li>
-
-    <li>  <label>Doğum Tarihi :</label> 27.08.1988</li>
-
-    <li>  <label>Uyruk :</label> T.C.</li>
-
-    </ul>
-
-    <br/><br/>
-    <b><div class="baslik-yani-uzatici"></div>Lisans Bilgileri</b>
-    <hr/>
-    <ul class="yanindaformaresmi">
-
-    <li>  <label>Kulübü :</label> Körükspor</li>
-
-    <li>  <label>Pozisyon :</label> AM R/C/L</li>
-
-    <li>  <label>Değer :</label> 41.000.000 $</li>
-
-    </ul>
-    <div id="formadanumaram">27</div>
-
-</div>
+            <div class="news-header"> <h3><i class="fa fa-newspaper-o"></i> Oyuncu Bilgileri</h3></div>
 
 
 
+            <div class="oyuncu-sol-resim">
+                <img src="images/oyuncu/ronaldinho.jpg" width="246" height="300"/>
+
+            </div>
+            <div class="sakatlanmis-oyuncu">Skt</div>
+
+            <div class="oyuncu-detay-yazi">
+
+
+                <h1>MURATCAN TAVUKÇUOĞLU</h1>
+
+                <b><div class="baslik-yani-uzatici"></div>Kişisel Bilgiler</b>
+                <hr/>
+                <ul>
+                    <li>  <label>Doğum Yeri :</label> İstanbul</li>
+
+                    <li>  <label>Boy :</label> 1.90 m</li>
+
+                    <li>  <label>Kilo :</label> 90 kg</li>
+
+                    <li>  <label>Yaş :</label> 25</li>
+
+                    <li>  <label>Doğum Tarihi :</label> 27.08.1988</li>
+
+                    <li>  <label>Uyruk :</label> T.C.</li>
+
+                </ul>
+
+                <br/><br/>
+                <b><div class="baslik-yani-uzatici"></div>Lisans Bilgileri</b>
+                <hr/>
+                <ul class="yanindaformaresmi">
+
+                    <li>  <label>Kulübü :</label> Körükspor</li>
+
+                    <li>  <label>Pozisyon :</label> AM R/C/L</li>
+
+                    <li>  <label>Değer :</label> 41.000.000 $</li>
+
+                </ul>
+                <div id="formadanumaram">27</div>
+
+            </div>
 
 
 
-</div>
 
 
-</div>
 
-<?php include "components/side-panel.php";?>
+        </div>
+
+
+    </div>
+
+    <?php include "components/side-panel.php";?>
 
 </div>
