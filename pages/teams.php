d58cadb76600c4e05fc16f24c3ffa9b261032a33
unknown <muratbayri@Toshiba.YELLOWPAGES.COM>
2015-07-31 14:55:03 +0300

    takmmmmm

--- pages/teams.php
+++ pages/teams.php
@@ -6,10 +6,17 @@
 
 <div class="news-header"> <h3><i class="fa fa-th-list"></i> Takımlar</h3></div>
 
+<?php
+
+    $goster = mysql_query("select * from takimlar");
+
+while ($oku = mysql_fetch_array($goster)){
+?>
+
 <div class="col-xs-6">
 <div class="oyuncu-liste">
-       <a href="index.php?page=takim-detay"><img src="images/fenerlogo.png" width="100" height="100"/></a>  <br/>
-       <p><a href="index.php?page=takim-detay">Körükspor</a></p>
+       <a href="index.php?page=takim-detay&takim=<?php echo $oku['takim_adiseo'] ?>"><img src="images/fenerlogo.png" width="100" height="100"/></a>  <br/>
+       <p><a href="index.php?page=takim-detay&takim=<?php echo $oku['takim_adiseo'] ?>"><?php echo $oku['takim_adi'] ?></a></p>
 
         <div class="oyuncu-adi">
 
@@ -43,42 +50,7 @@
     </div>
         </div>
 
-         <div class="col-xs-6">
-             <div class="oyuncu-liste">
-       <img src="images/fenerlogo.png" width="100" height="100"/>  <br/>
-       <p>Körükspor</p>
-
-        <div class="oyuncu-adi">
-
-
-            <table class="table">
-
-                      <tbody>
-                        <tr>
-                          <th scope="row">1</th>
-                          <td><div class="sakatlanmis-oyuncu">Skt</div></td>
-                          <td>OZAN</td>
-                        </tr>
-
-                        <tr>
-                          <th scope="row">2</th>
-                          <td><div class="yabanci-oyuncu">Ybn</div></td>
-                          <td>RONALDO</td>
-                        </tr>
-
-                           <tr>
-                          <th scope="row">2</th>
-                          <td><div class="yerli-oyuncu">Yrl</div></td>
-                          <td>RONALDO</td>
-                        </tr>
-
-                      </tbody>
-            </table>
-
-
-        </div>
-    </div>
-        </div>
+        <?php } ?>
 
 
 </div>
