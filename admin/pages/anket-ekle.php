acdfdb4e15c789f0ac6ae10d71bcc998f0a8b1b6
unknown <muratbayri@Toshiba.YELLOWPAGES.COM>
2015-08-05 09:12:14 +0300

    anket güncelleme tamamlandı

--- admin/pages/anket-ekle.php
+++ admin/pages/anket-ekle.php
@@ -26,7 +26,7 @@ if($kontrol)
 
 
 
-mysql_query("insert into anketler(anket_baslik,baslik_seo,deger1,deger2,deger3,deger1_say,deger2_say,deger3_say,anasayfa) values('$baslik','$baslikseo','$bir','$iki','$uc','0','0','0','$anasayfa')",$baglanti) or die("Veri eklenemedi".mysql_error());
+mysql_query("insert into anket(anket_baslik,baslik_seo,deger1,deger2,deger3,deger1_say,deger2_say,deger3_say,anasayfa) values('$baslik','$baslikseo','$bir','$iki','$uc','0','0','0','$anasayfa')",$baglanti) or die("Veri eklenemedi".mysql_error());
 
 echo "
 
